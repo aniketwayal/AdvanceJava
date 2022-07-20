2ffecd75e07dde5b41eda2392c2d2a555f948515
Aniket Navnath Wayal <33562487+aniketwayal@users.noreply.github.com>
2022-07-20 13:25:31 +0530

    Add files via upload

---
+++ ServletConfigEg/src/main/java/com/DemoServlet.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		ServletConfig cfg= getServletConfig();
+		String d = cfg.getInitParameter("mysqldriver");
+		out.println("Driver name is : " + d);
+	}
+
+}
